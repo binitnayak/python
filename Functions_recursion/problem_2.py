29972a5423471bd51c942d59f8ce9d3ebfb67acc
Binit Nayak <binitnayak00@gmail.com>
2026-07-08 15:40:44 +0530

    adding problem sets question

--- Functions_recursion/problem_2.py
+++ Functions_recursion/problem_2.py
@@ -1,4 +1,4 @@
 def f_to_c(f):
   return 5*(f-32)/9
 f=int(input("enter temperature in F: "))
-print(f_to_c(f))
+print(f"{f_to_c(f)} Degree of C")
---
+++ Functions_recursion/problem_5.py
@@ -0,0 +1,6 @@
+def  multi(n):
+    for i in range(1,n+1):
+        print(f"{n} x {i} = {n*i}")
+
+
+    multi(5)
